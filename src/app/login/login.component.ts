fd9eaeca0cb560b945eda18242bead5616040f48
Nithin <nithinravindran1990@gmail.com>
2023-05-14 12:06:15 +0530

    update_1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,17 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   // data = "Your perfect banking partner "
-  // uname: any;  
+  uname: any;  
+  psw:any
 
   constructor(){}
   ngOnInit(): void { }
   
-  login() {
-    alert("you have logged in")
+  login(uname: any, psw: any) {
+    // this.uname = uname.value;                # template rending variable 
+    // this.psw = psw.value;                        #varibale name in html to acccess
+    // console.log(this.uname, this.psw);             to access the data from html to view
+    alert("you have logged in")                         
   }
 
   unamechange(event:any ) {
-    // console.log(event.target.value);  1st method
-    // this.uname=(event.target.value)  2nd method data binding
+    // console.log(event.target.value);  1st method event bindiing  (click)
+    // this.uname=(event.target.value)  2nd method  $(event) data binding  (change)
   }
 }
